5d4602fb955a299ddff7dc3ebdb6fa5789f799d3
breezybrow <browder.bree@gmail.com>
2021-05-20 20:38:01 +0000

    Console Basics, Need to add FileStorage

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This is the entry point of our command interpreter """
+
+import cmd
+import models
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """ This is our console """
+
+    prompt = '(hbnb)'
+
+    def emptyline(self):
+        """ Do nothing """
+        pass
+
+    def do_quit(self, line):
+        """ Command to exit program """
+        return True
+
+    def do_EOF(self, line):
+        """ exits at EOF """
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
