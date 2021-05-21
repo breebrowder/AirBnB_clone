8e7289cdd5750a9991d61da48f29a267bc6776e2
breezybrow <browder.bree@gmail.com>
2021-05-21 03:18:15 +0000

    Making new class for file storage, adding attr and methods

---
+++ models/engine/file_storage.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+""" In this module we will create a class to manage File Storage """
+
+from models import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import json
+from os import path
+
+
+class FileStorage:
+    """ This class will manage storage for multiple classes """
+
+    __file_path = 'file.json'
+    __objects = {}
+    classes = {"BaseModel": BaseModel,
+               "User": User,
+               "City": City,
+               "State": State,
+               "Amenity": Amenity,
+               "Place": Place,
+               "Review": Review}
+
+    def all(self):
+        """ Returns the dict __objects """
+        return self.__objects
+
+    def new(self, obj):
+        """ Sets in __objects the obj with key <obj class name>.id """
+        FileStorage.__objects["{}.{}".format
+                              (obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        """ Serializes __objects to the JSON file """
+        new_Dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_Dict[key] = value.to_dict()
+
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as f:
+            f.write(json.dumps(new_Dict))
+
+    def reload(self):
+        """ Deserializes the JSON file to __objects """
+        if path.isfile(self.__file_path):
+            with open(FileStorage.__file_path, mode="r") as f:
+                myFile = json.load(f)
+                for key, value in myFile.items():
+                    new_Items = myFile[key]["__class__"]
+                    new_Class = self.classes[new_Items]
+                    self.__objects[key] = new_Class(**value)
+        if path.isfile(self.__file_path) is False:
+            return
