b3ac41de993ebdfaeac9f5b7e2db4d2dc7fef18e
breezybrow <browder.bree@gmail.com>
2021-05-21 17:06:52 +0000

    Creating instance for filestorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """
 This module creates a unique FileStorage instance
@@ -6,6 +5,7 @@ for your application
 """
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-=======
->>>>>>> 53135dc6d86a0e0aa17ba796c7286e53c4cb7f1b
 
+
+storage = FileStorage()
+storage.reload()
