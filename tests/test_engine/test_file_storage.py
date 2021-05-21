0b5ed4413bb400494f026ec594c83ea56099812e
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-21 22:38:13 +0000

    Adding test files

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A module for unittests for the base model class"""
+import unittest
+import os
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+fs = FileStorage()
+class TestBaseModel(unittest.TestCase):
+    """A class to test BaseModel & File storage"""
+    def test_filestorage_save(self):
+        if os.path.exists(fs._FileStorage__file_path):
+            os.remove(fs._FileStorage__file_path)
+        bm1 = BaseModel()
+        bm1.save()
+        self.assertNotEqual(os.path.getsize(fs._FileStorage__file_path), 0)
+    def test_filestorage_new(self):
+        bm1 = BaseModel()
+        self.assertNotEqual(len(fs._FileStorage__objects), 0)
+    def test_filestorage_all(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(fs.all(), dict)
