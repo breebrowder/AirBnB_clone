d1d8140e450169c335741be5277f9230fce324f0
breezybrow <browder.bree@gmail.com>
2021-05-22 20:41:38 +0000

    Console functions added, pep8 compli, exexutes

--- console.py
+++ console.py
@@ -40,7 +40,8 @@ class HBNBCommand(cmd.Cmd):
         """ create new inst of BaseModel, save to the JSON file, prints ID """
         if line:
             if line in HBNBCommand.classes:
-                newStance = HBNBCommand.classes.get(line)
+                cls_to_ins = HBNBCommand.classes.get(line)
+                newStance = cls_to_ins()
                 newStance.save()
                 print(newStance.id)
             else:
@@ -79,6 +80,46 @@ class HBNBCommand(cmd.Cmd):
         else:
             models.storage.all().pop(space_arg[0] + '.' + space_arg[1], None)
             models.storage.save()
+
+    def do_all(self, line):
+        """ Prints all str repr of instances based or not on the cls name """
+        space_arg = line.split(' ')
+        arg = ""
+        str_list = []
+        if line == "":
+            for key, value in models.storage.all().items():
+                arg = str(value)
+                str_list.append(string)
+            print(str_list)
+        elif space_arg[0] not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        else:
+            for key, value in models.storage.all().items():
+                if value.__class__.__name__ == space_arg[0]:
+                    arg = str(value)
+                    str_list.append(arg)
+            print(str_list)
+
+    def do_update(self, line):
+        """ Updates an inst based on cls name & id by adding/updating attr """
+        space_arg = line.split(' ')
+        if line == "":
+            print("** class name missing **")
+        elif space_arg[0] not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        elif len(space_arg) < 2:
+            print("** instance id missing **")
+        elif space_arg[0] + '.' + space_arg[1] not in \
+                models.storage.all().keys():
+            print("** no instance found **")
+        elif len(space_arg) < 3:
+            print("** attribute name missing **")
+        elif (space_arg) < 4:
+            print("** attribute name missing **")
+        else:
+            obj = models.storage.all().get(space_arg[0] + '.' + space_arg[1])
+            setattr(obj, space_arg[2], space_arg[3][1:-1])
+            obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
