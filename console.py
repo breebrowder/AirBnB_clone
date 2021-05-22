0dd25c3e4cb51df5da3f7d61fdb8f920f3b26403
breezybrow <browder.bree@gmail.com>
2021-05-22 19:32:54 +0000

    Command Interpreter updates

--- console.py
+++ console.py
@@ -3,12 +3,26 @@
 
 import cmd
 import models
+from models import BaseModel
+from models.city import City
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """ This is our console """
 
     prompt = '(hbnb)'
+    classes = {"BaseModel": BaseModel,
+               "City": City,
+               "User": User,
+               "State": State,
+               "Place": Place,
+               "Amenity": Amenity,
+               "Review": Review}
 
     def emptyline(self):
         """ Do nothing """
@@ -21,6 +35,50 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """ exits at EOF """
         return True
+
+    def do_create(self, line):
+        """ create new inst of BaseModel, save to the JSON file, prints ID """
+        if line:
+            if line in HBNBCommand.classes:
+                newStance = HBNBCommand.classes.get(line)
+                newStance.save()
+                print(newStance.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_show(self, line):
+        """ Prints the string repr of an inst based on class name & id """
+        space_arg = line.split(' ')
+        if line == "":
+            print("** class name missing **")
+        elif space_arg[0] not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        elif len(space_arg) < 2:
+            print("** instance id missing **")
+        elif space_arg[0] + '.' + space_arg[1] not in \
+             models.storage.all().keys():
+            print("** no instance found **")
+        else:
+            obj = models.storage.all().get(space_arg[0] + '.' + space_arg[1])
+            print(obj)
+
+    def do_destroy(self, line):
+        """  Deletes an instance based on the class name and id """
+        space_arg = line.split(' ')
+        if line == "":
+            print("** class name missing **")
+        elif space_arg[0] not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        elif len(space_arg) < 2:
+            print("** instance id missing **")
+        elif space_arg[0] + '.' + space_arg[1] not in \
+             models.storage.all().keys():
+            print("** no instance found **")
+        else:
+            models.storage.all().pop(space_arg[0] + '.' + space_arg[1], None)
+            models.storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
